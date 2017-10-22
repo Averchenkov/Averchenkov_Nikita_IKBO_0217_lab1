98bba808ee7a41c77b8ff69dd51d71566ed46816
Averchenkov Nikita IKBO-02-17 <31746573+Averchenkov@users.noreply.github.com>
2017-10-22 17:46:32 +0300

    Add files via upload

--- lab1.c
+++ lab1.c
@@ -2,32 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-float c_to_f(float c){              //Перевод из Целсии в Фаренгейты
+float c_to_f(float c){              //Celsius -> Fahrenheit
     float f = c*9/5+32;
     return (f);
 }
 
-float c_to_k(float c){              //Перевод из Целсии в Кельвины
+float c_to_k(float c){              //Celsius -> Kelvin
     float k = c+273.15;
     return k;
 }
 
-float f_to_c(float f){              //Перевод из Фаренгейт в Целсии
+float f_to_c(float f){              //Fahrenheit -> Celsius
     float c = (f-32)*5/9;
     return c;
 }
 
-float f_to_k(float f){              //Перевод из Фаренгейт в Кельвины
+float f_to_k(float f){              //Fahrenheit -> Kelvin
     float k = c_to_k(f_to_c(f));
     return k;
 }
 
-float k_to_c(float k){              //Перевод из Кельвин в Целсии
+float k_to_c(float k){              //Kelvin -> Celsius
     float c = k-273.15;
     return c;
 }
 
-float k_to_f(float k){              //Перевод из Кельвин в Фаренгейты
+float k_to_f(float k){              //Kelvin -> Fahrenheit
     float f = c_to_f(k_to_c(k));
     return f;
 }
@@ -36,33 +36,33 @@ float k_to_f(float k){              //Перевод из Кельвин в Фаренгейты
 
 int main(int argc,char *argv[])
 {
-    if (argc == 1){                         //Проверка на наличие какмх-либо вводимых значений
+    if (argc == 1){                         //Check for input values
         printf("Enter the values");
         return 1;
     }else{
-        float i = atof(argv[1]);            //Перевод в числа в тип float
+        float i = atof(argv[1]);            //Conversion to float
 
-        switch(argc){                          //Смотрим сколько преданно аргументов
+        switch(argc){                          //How many values
 
 
 
-            case 2:                        //Если 2 то число представляем в трех шкалах и делаем конвертацию
-                if (c_to_k(i) < 0) printf("Can't be such a temperature in Celsius\n\n");  //Проверка абсолютного нуля
+            case 2:                        //If 2 then translate for 3 scales
+                if (c_to_k(i) < 0) printf("Can't be such a temperature in Celsius\n\n");  //Check for absolute zero
                 else{
-                    printf("%.2f F\n",i);       //Конвертация для Цельсии
+                    printf("%.2f F\n",i);       //Translate from Celsius
                     printf("%.2f C\n",c_to_f(i));
                     printf("%.2f K\n\n",c_to_k(i));
                 }
 
-                if (f_to_k(i) < 0) printf("Can't be such a temperature in Fahrenheit\n\n");  //Проверка абсолютного нуля
+                if (f_to_k(i) < 0) printf("Can't be such a temperature in Fahrenheit\n\n");  //Check for absolute zero
                 else{
-                    printf("%.2f F\n",i);       //Конвертация для Фаренгейт
+                    printf("%.2f F\n",i);       //Translate from Fahrenheit
                     printf("%.2f C\n",f_to_c(i));
                     printf("%.2f K\n\n",f_to_k(i));
                 }
-                if (i < 0) printf("Can't be such a temperature in Kelvin\n"); //Проверка абсолютного нуля
+                if (i < 0) printf("Can't be such a temperature in Kelvin\n"); //Check for absolute zero
                 else{
-                    printf("%.2f K\n",i);       //Конвертация для Кельвин
+                    printf("%.2f K\n",i);       //Translate from Kelvin
                     printf("%.2f C\n",k_to_c(i));
                     printf("%.2f F\n",k_to_f(i));
                 }
@@ -70,13 +70,13 @@ int main(int argc,char *argv[])
 
 
 
-            case 3:                     //Если 2 то смотрим в какой шкале заданно число
-            if (strlen(argv[2])==1){    //Проверка то, что ползователь ввел один символ
+            case 3:                     //If 3 then checking 2 values
+            if (strlen(argv[2])==1){    //Check for 1 symbol
                switch(*argv[2]){
 
 
-                    case 'C':           //Если в Цельсии то делаем конвертацию для Цельсии
-                        if (c_to_k(i) < 0) printf("Can't be such a temperature\n"); //Проверка абсолютного нуля
+                    case 'C':           //Translate from Celsius
+                        if (c_to_k(i) < 0) printf("Can't be such a temperature\n"); //Check for absolute zero
                         else {
                             printf("%.2f C\n",i);
                             printf("%.2f F\n",c_to_f(i));
@@ -85,8 +85,8 @@ int main(int argc,char *argv[])
                         break;
 
 
-                    case 'F':           //Если в Фаренгейтах то делаем конвертацию для Фаренгейт
-                        if (c_to_k(i) < 0) printf("Can't be such a temperature\n"); //Проверка абсолютного нуля
+                    case 'F':           //Translate from Fahrenheit
+                        if (c_to_k(i) < 0) printf("Can't be such a temperature\n"); //Check for absolute zero
                         else{
                             printf("%.2f F\n",i);
                             printf("%.2f,C\n"),f_to_k(i);
@@ -95,8 +95,8 @@ int main(int argc,char *argv[])
                         break;
 
 
-                    case 'K':           //Если в Кельвинах то делаем конвертацию для Кельвин
-                        if (c_to_k(i) < 0) printf("Can't be such a temperature\n"); //Проверка абсолютного нуля
+                    case 'K':           //Translate from Kelvin
+                        if (c_to_k(i) < 0) printf("Can't be such a temperature\n"); //Check for absolute zero
                         else{
                             printf("%.2f K\n",i);
                             printf("%.2f,C\n",k_to_c(i));
@@ -105,13 +105,13 @@ int main(int argc,char *argv[])
                         break;
 
 
-                    default:printf("Enter number and C,F or K");    //Если не заданна шкала
+                    default:printf("Enter number and C,F or K");    //If not scale is entered
                 }
             } else printf("Enter number and C,F or K\n");
             break;
 
 
-            default:printf("Enter number and scale\n");               //Если заданно болеше 2 аргументов
+            default:printf("Enter number and scale\n");               //If entered more then 2 values
         }
     }
     return 0;
